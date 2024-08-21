5d1e50ce409e2bbfe964e531a1d58146980d2703
priyanshu Arora <113926973+priyanshuArorra@users.noreply.github.com>
2024-08-21 18:40:55 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,153 @@
+import { TSP } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "SQL",
+
+    },
+
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "c/c++",
+
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+
+
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+
+    {
+        imageUrl: sass,
+        name: "Python",
+
+    },
+    {
+        imageUrl: nodejs,
+        name: "threejs",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: " Project Intern ",
+        company_name: "The Spark Foundation",
+        icon: TSP,
+        iconBg: "#accbe1",
+        date: "August 2024 - September 2024",
+        points: [
+            "Creating predictive models using supervised and unsupervised machine learning",
+            "Beginner, intermediate, and advanced levels.",
+            "Such as in sports analysis or retail store optimization for increased profits, etc.",
+
+        ],
+    },
+
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/priyanshuArorra',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/priyanshu-arora-321202251/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Automated Payroll System with image capturing',
+        description: 'Developed a payroll system with employee tracking, integrating image capture and GPS for accurate working hour calculation.',
+        link: 'https://github.com/priyanshuArorra/Automated-payroll-system-with-image-capture-and-gps-availability/tree/main',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'E-Yantra',
+        description: 'Built a responsive educational website for the college to promote robotics, providing comprehensive resources and information on robotics components.',
+        link: 'https://e-yantra-ten.vercel.app/',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Smart Traffic Detection and Signal Control Model',
+        description: 'Developed a real-time traffic signal control system for university campuses using computer vision and IoT, optimizing traffic flow and energy consumption.',
+        link: 'https://github.com/priyanshuArorra/Traffic_optimization_model/tree/main',
+    },
+
+];
