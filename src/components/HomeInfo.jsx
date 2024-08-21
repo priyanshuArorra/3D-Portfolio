12c22cd9be3ddcac7bd642ecdfc48fcd4a3afda2
priyanshu Arora <113926973+priyanshuArorra@users.noreply.github.com>
2024-08-21 18:54:15 +0530

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,10 +7,10 @@ const HomeInfo = ({ currentStage }) => {
         return (
             <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
                 Hi, I'm
-                <span className='font-semibold mx-2 text-white'>Priyanshu</span>
+                <span className='font-semibold mx-2 text-white'>Priyanshu Arora</span>
                 👋
                 <br />
-                A Pre-final year student in SRMIST
+                A Pre-final year student at SRMIST, Chennai
             </h1>
         );
 
